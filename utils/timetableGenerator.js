1bcb920c676faeaef54fe19d5bea67b7692f05a2
HemanthRaj0C <personalhemanthraj@gmail.com>
2025-03-16 11:29:21 +0530

    [Fix] Timetable

--- utils/timetableGenerator.js
+++ utils/timetableGenerator.js
@@ -13,14 +13,13 @@ const generateTimetable = async (params) => {
 
     // Default time slots (1 hour each)
     const defaultTimeSlots = [
+        { start: '08:00', end: '09:00' },
         { start: '09:00', end: '10:00' },
         { start: '10:00', end: '11:00' },
         { start: '11:00', end: '12:00' },
         { start: '12:00', end: '13:00' },
         { start: '13:00', end: '14:00' },
         { start: '14:00', end: '15:00' },
-        { start: '15:00', end: '16:00' },
-        { start: '16:00', end: '17:00' }
     ];
 
     // Use only the required number of slots per day
